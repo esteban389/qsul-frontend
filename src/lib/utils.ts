1e477151481b7c55810df2b9cc11ab043aaa6a04
Esteban <estebabmur@gmail.com>
2025-04-04 18:31:15 -0500

    Add downloadURI function to facilitate file downloads from a given URI

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -251,3 +251,12 @@ export const jsonToSvgDataUrl = (
     return '';
   }
 };
+
+export function downloadURI(uri:string, name:string) {
+  let link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
