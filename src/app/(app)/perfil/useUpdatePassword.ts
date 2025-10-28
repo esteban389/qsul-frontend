e794e2030531d5523ea14b4d5fbde73850aa7c74
esteban389 <103962731+esteban389@users.noreply.github.com>
2025-10-28 18:42:27 -0500

    feat: add success toast notification for password update
    - Integrated a success toast notification using Sonner upon successful password update, enhancing user feedback and experience during the profile management process.

--- src/app/(app)/perfil/useUpdatePassword.ts
+++ src/app/(app)/perfil/useUpdatePassword.ts
@@ -1,6 +1,7 @@
 import backendClient from "@/services/backendClient";
 import { UpdatePasswordRequest } from "@/types/user";
 import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 async function mutate(request: UpdatePasswordRequest) {
   return backendClient.post('/api/profile/password', request);
@@ -11,5 +12,6 @@ export default function useUpdatePassword(request: UpdatePasswordRequest) {
   return useMutation({
     mutationFn: () => mutate(request),
     mutationKey: ['updatePassword'],
+    onSuccess: ()=> toast.success('Contraseña actualizada')
   })
 }
